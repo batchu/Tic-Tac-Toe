526e193abb2a42cb9a20ddaa4dd99921d2ba0415
batchu <batchu@batchu.net>
2017-11-18 15:19:29 -0500

    Proper Enzyme setup complete. Working on implementing test cases using Enzyme.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {App, Board, Game, Square} from './App';
-
 import * as enzyme from 'enzyme';
+import * as ReactTestRenderer  from 'react-test-renderer';
+import * as Adapter from 'enzyme-adapter-react-16';
 
-import * as  ReactTestRenderer from 'react-test-renderer';
+import  App from './App'
+import { Board, Game, Square, Link} from './App';
 
-it('App renders without crashing', () => {
+test('App renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App/>, div);
 
 });
 
-it('Game renders properly', () => {
+test('Game renders properly', () => {
     const component = ReactTestRenderer.create(
         <Game/>
     )
@@ -21,7 +22,7 @@ it('Game renders properly', () => {
 
 })
 
-it("Board's handleClick should update the squares array based on the value passed to it", () => {
+test.skip("Board's handleClick should update the squares array based on the value passed to it", () => {
 
     const instance: Board = new Board()
     instance.handleClick(5)
@@ -29,15 +30,14 @@ it("Board's handleClick should update the squares array based on the value passe
 
 })
 
-it("When a Square is clicked, it should invoke the function attached to the click event", () => {
+test("When a Square is clicked, it should invoke the function attached to the click event", () => {
 
     const squareValue = 'X'
     let onClickwasCalled: boolean = false
     const onClick = () => {
         onClickwasCalled = true
     }
-    enzyme.configure({adapter: new enzyme.EnzymeAdapter()})
-
+    enzyme.configure({adapter: new Adapter()})
     const instance = enzyme.shallow(<Square
         value={squareValue}
         onClick={onClick}
@@ -47,3 +47,27 @@ it("When a Square is clicked, it should invoke the function attached to the clic
     expect(onClickwasCalled).toBeTruthy()
 
 })
+
+test('Link changes the class when hovered', () => {
+    const component = ReactTestRenderer.create(
+      <Link page="http://www.facebook.com">Facebook</Link>
+    );
+    let tree = component.toJSON();
+  
+    expect(tree).toMatchSnapshot();
+  
+    // manually trigger the callback
+    if(tree!=null)
+    tree.props.onMouseEnter();
+    // re-rendering
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  
+    // manually trigger the callback
+    if(tree!=null)
+    tree.props.onMouseLeave();
+    // re-rendering
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import './App.css';
 
 interface SquareProps {
-    value:string;
-    onClick:()=>void
+    value: string;
+    onClick: () => void
 }
-class Square extends React.Component <SquareProps, {}> {
+class Square extends React.Component<SquareProps, {}> {
     render() {
         return (
             <button
@@ -23,10 +23,10 @@ interface BoardProps {
 }
 
 interface BoardState {
-    squares:Array<string>
+    squares: Array<string>
 }
 
-class Board extends React.Component <BoardProps, BoardState> {
+class Board extends React.Component<BoardProps, BoardState> {
 
     constructor() {
         super()
@@ -35,17 +35,17 @@ class Board extends React.Component <BoardProps, BoardState> {
         }
     }
 
-    handleClick(i:number) {
+    handleClick(i: number) {
         const squares = this.state.squares.slice();
         squares[i] = 'X';
-        this.setState({squares: squares})
+        this.setState({ 'squares': squares })
     }
 
-    renderSquare(i:number) {
+    renderSquare(i: number) {
         return (
             <Square
                 value={this.state.squares[i]}
-                onClick={()=> this.handleClick(i)}
+                onClick={() => this.handleClick(i)}
             />
         );
     }
@@ -82,15 +82,64 @@ class Game extends React.Component {
     }
 }
 
-class App extends React.Component {
+interface LinkProps {
+    page: string
+}
+
+interface LinkState {
+    class: STATUS
+}
+
+enum STATUS {
+    NORMAL = 'NORMAL',
+    HOVERED = 'HOVERED'
+}
+
+class Link extends React.Component<LinkProps, LinkState> {
+
+    constructor(props: LinkProps) {
+        super(props);
+
+        this._onMouseEnter = this._onMouseEnter.bind(this);
+        this._onMouseLeave = this._onMouseLeave.bind(this);
+
+        this.state = {
+            class: STATUS.NORMAL,
+        };
+    }
+
+    _onMouseEnter() {
+        this.setState({ class: STATUS.HOVERED });
+    }
+
+    _onMouseLeave() {
+        this.setState({ class: STATUS.NORMAL });
+    }
+
     render() {
         return (
-            <Game />
+            <a
+                className={this.state.class}
+                href={this.props.page || '#'}
+                onMouseEnter={this._onMouseEnter}
+                onMouseLeave={this._onMouseLeave}
+            >
+                {this.props.children}
+            </a>
+        );
+    }
+
+}
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Link page="https://batchu.net"> Batchu</Link>
         );
     }
 }
 
-export {App}
-export {Game}
-export {Board}
-export {Square}
+export { Game }
+export { Board }
+export { Square }
+export { Link }
