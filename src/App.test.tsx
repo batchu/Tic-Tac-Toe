c1d08cd7ed9e3469fb047b6d0d5c1b3f9f55b6b0
batchu <batchu@batchu.net>
2017-11-19 14:25:09 -0500

    Refactored tests to create suits for each component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,55 +7,61 @@ import * as Adapter from 'enzyme-adapter-react-16';
 import App from './App'
 import { Board, Game, Square } from './App';
 
+
 //Run once before all tests
-beforeAll(()=>{
+beforeAll(() => {
     enzyme.configure({ adapter: new Adapter() })
 })
 
-//An elementary test to verify that the entire App loads without a hiccup
-test('App renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-
-});
-
-//A snapshot test to verify the Game component's UI did not change unexpectedly after being loaded
-//More at https://facebook.github.io/jest/docs/en/snapshot-testing.html
-test('Game renders properly', () => {
-    //Create an instance of the Component
-    const component = ReactTestRenderer.create(
-        <Game />
-    )
-
-    //Retreive it's JSON
-    const tree = component.toJSON()
-
-    //Compare it with the stored snapshot
-    expect(tree).toMatchSnapshot()
-
+describe('<App />', () => {
+    //An elementary test to verify that the entire App loads without a hiccup
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+    });
 })
 
-test.skip("Board's handleClick should update the squares array based on the value passed to it", () => {
+describe('<Game />', () => {
+    //A snapshot test to verify the Game component's UI did not change unexpectedly after being loaded
+    //More at https://facebook.github.io/jest/docs/en/snapshot-testing.html
+    it('renders properly', () => {
+        //Create an instance of the Component
+        const component = ReactTestRenderer.create(
+            <Game />
+        )
 
-    const instance: Board = new Board()
-    instance.handleClick(5)
-    expect(instance.state.squares[5]).toEqual('X')
+        //Retreive it's JSON
+        const tree = component.toJSON()
 
+        //Compare it with the stored snapshot
+        expect(tree).toMatchSnapshot()
+
+    })
 })
 
-test("When a Square is clicked, it should invoke the function attached to the click event", () => {
+describe('<Board />', () => {
+    it("Board's handleClick should update the squares array based on the value passed to it", () => {
+            const wrapper = enzyme.shallow(<Board />)
+            expect(wrapper.find('Square').length).toEqual(9)
+        })
+})
 
-    //Create the necessary input values for the test case
-    const squareValue = 'X'
-    const mockOnClick = jest.fn()
 
-    const wrapper  = enzyme.shallow(<Square
-        value={squareValue}
-        onClick={mockOnClick}
-    />)
+describe('<Square />', () => {
+    it("when click it should invoke the function attached to the click event", () => {
 
-    wrapper.find('button').simulate('click')
-    expect(wrapper.find("button").props().children).toEqual('X')
-    expect(mockOnClick.mock.calls.length).toBe(1)
+        //Create the necessary input values for the test case
+        const squareValue = 'X'
+        const mockOnClick = jest.fn()
 
+        const wrapper = enzyme.shallow(<Square
+            value={squareValue}
+            onClick={mockOnClick}
+        />)
+
+        wrapper.find('button').simulate('click')
+        expect(wrapper.find("button").props().children).toEqual('X')
+        expect(mockOnClick.mock.calls.length).toBe(1)
+
+    })
 })
